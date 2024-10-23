ae7cc8181c67b04bf36b39225e3fc755de499067
JuanDiego2004 <peraltaarrietajuan123@gmail.com>
2024-10-23 18:04:55 -0500

    prueba 4

--- sistema-pos-java/src/main/java/com/sistema/pos/sistema/pos/java/SistemaPosJava.java
+++ sistema-pos-java/src/main/java/com/sistema/pos/sistema/pos/java/SistemaPosJava.java
@@ -12,7 +12,7 @@ public class SistemaPosJava {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        System.out.println("Hello World Junior!");
+        System.out.println("Hello World Juniodfr!");
     }
 }
 
